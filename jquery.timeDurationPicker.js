3384177f207fa61a2a94bff8cfa5971b6cb4fb23
Nikolay Digaev <ffs.cmp@gmail.com>
2015-12-02 12:29:43 +0500

    Fix options merge & some minor optimization

--- jquery.timeDurationPicker.js
+++ jquery.timeDurationPicker.js
@@ -76,6 +76,7 @@
         human_hours: 'hours',
         human_minutes: 'minutes',
         human_seconds: 'seconds',
+        and: 'and',
         button_ok: 'Done'
       },
       ru: {
@@ -91,13 +92,14 @@
         human_hours: 'часов',
         human_minutes: 'минут',
         human_seconds: 'секунд',
+        and: 'и',
         button_ok: 'Выбрать'
       }
     },
     _create: function() {
         var self = this;
 
-        this.options = $.extend($.timeDurationPicker.defaults, this.options);
+        this.options = $.extend({}, $.timeDurationPicker.defaults, this.options);
 
         this._content = {};
         this._content.div = $('<div />');
@@ -128,7 +130,7 @@
         this._content.button.on('click', function(e) {
           self._content.div.fadeOut();
           if (self.options.onselect) {
-            self.options.onselect(self.getDuration(), self.getHumanDuration());
+            self.options.onselect(self.element, self.getDuration(), self.getHumanDuration());
           }
         });
         this._content.button.appendTo(this._content.div);
@@ -296,39 +298,33 @@
       return seconds;
     },
     getHumanDuration: function() {
+      var units = [];
       var duration = '';
+
       if (this.options.years && this.getYears() > 0) {
-        duration += this.getYears() + ' ' + this._tr('human_years');
+        units.push({value: this.getYears(), name: this._tr('human_years')});
       }
       if (this.options.months && this.getMonths() > 0) {
-        if (duration) {
-          duration += ', ';
-        }
-        duration += this.getMonths() + ' ' + this._tr('human_months');
+        units.push({value: this.getMonths(), name: this._tr('human_months')});
       }
       if (this.options.days && this.getDays() > 0) {
-        if (duration) {
-          duration += ', ';
-        }
-        duration += this.getDays() + ' ' + this._tr('human_days');
+        units.push({value: this.getDays(), name: this._tr('human_days')});
       }
       if (this.options.hours && this.getHours() > 0) {
-        if (duration) {
-          duration += ', ';
-        }
-        duration += this.getHours() + ' ' + this._tr('human_hours');
+        units.push({value: this.getHours(), name: this._tr('human_hours')});
       }
       if (this.options.minutes && this.getMinutes() > 0) {
-        if (duration) {
-          duration += ', ';
-        }
-        duration += this.getMinutes() + ' ' + this._tr('human_minutes');
+        units.push({value: this.getMinutes(), name: this._tr('human_minutes')});
       }
       if (this.options.seconds && this.getSeconds() > 0) {
-        if (duration) {
-          duration += ', ';
-        }
-        duration += this.getSeconds() + ' ' + this._tr('human_seconds');
+        units.push({value: this.getSeconds(), name: this._tr('human_seconds')});
+      }
+
+      for (var i = 0, l = units.length; i < l; ++i) {
+        var unit = units[i];
+        var separator = i == l - 1 ? '' : (i == l - 2 ? ' ' + this._tr('and') + ' ' : ', ');
+
+        duration += unit.value + ' ' + unit.name + separator;
       }
       return duration;
     }
