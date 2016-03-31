11d5eef3e8655507f70b5d2eb44f552108ea88d3
Nikolay Digaev <ffs.cmp@gmail.com>
2016-03-31 15:16:47 +0500

    Fix for fixed position

--- jquery.timeDurationPicker.js
+++ jquery.timeDurationPicker.js
@@ -33,6 +33,8 @@
         if (inst.element[j] == e.target) {
           var offset = $(e.target).offset();
           offset.top += $(e.target).outerHeight();
+          offset.top -= $(window).scrollTop();
+          offset.left -= $(window).scrollLeft();
           inst._content.div.css(offset).fadeIn();
         }
       }
